064efd1ed5837da57984c5c396333f312e507398
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-04-16 11:00:33 +0530

    Created the studentSchema

--- models/student/studentModel.js
+++ models/student/studentModel.js
@@ -1,3 +1,39 @@
 /**
  * Created by Yash 1300 on 16-04-2018.
  */
+
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    favTeachers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Teacher'
+        }
+    ]
+});
+
+module.exports = mongoose.model('Student', studentSchema, "students");
