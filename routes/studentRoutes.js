915f0441f98a2f71518a248a796cc8d8a835aedb
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-04-20 04:00:16 +0530

    Added routes for fetching details and added a token verification check before executing any route

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,3 +1,22 @@
 /**
  * Created by Yash 1300 on 16-04-2018.
  */
+
+const StudentController = require('../controllers/studentController');
+const router = require('express').Router();
+
+const secret = process.env.SECRET;
+
+router.use((req, res, next) => {
+    let token = req.headers['x-access-token'];
+    StudentController.verifyToken(token, secret).then(data => {
+        req.decoded = data;
+        next();
+    }).catch(err => res.json(err));
+});
+
+router.get('/fetchDetails', (req, res) => {
+    StudentController.fetchDetails(req.decoded._id).then(data => res.json(data)).catch(err => res.json(err));
+});
+
+module.exports = router;
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,3 +1,22 @@
 /**
  * Created by Yash 1300 on 16-04-2018.
  */
+
+const TeacherController = require('../controllers/teacherController');
+const router = require('express').Router();
+
+const secret = process.env.SECRET;
+
+router.use((req, res, next) => {
+    let token = req.headers['x-access-token'];
+    TeacherController.verifyToken(token, secret).then(data => {
+        req.decoded = data;
+        next();
+    }).catch(err => res.json(err));
+});
+
+router.get('/fetchDetails', (req, res) => {
+    TeacherController.fetchDetails(req.decoded._id).then(data => res.json(data)).catch(err => res.json(err));
+});
+
+module.exports = router;
