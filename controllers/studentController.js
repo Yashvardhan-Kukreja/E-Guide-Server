a602673fc40a51c907a8ff7ac46353adeefd66ef
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-04-20 03:59:29 +0530

    Added controllers for token verification and fetching personal details

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,3 +1,35 @@
 /**
  * Created by Yash 1300 on 16-04-2018.
  */
+
+const StudentTransactions = require('../models/student/studentTransactions');
+const jwt = require('jsonwebtoken');
+const Promise = require('bluebird');
+
+module.exports.verifyToken = (token, secret) => {
+    return new Promise((resolve, reject) => {
+        if (token) {
+            jwt.verify(token, secret, (err, decoded) => {
+                if (err)
+                    reject({success: false, message: "An error occurred"});
+                else {
+                    decoded ? resolve(decoded) : reject({success: false, message: "Corrupted token provided"});
+                }
+            });
+        } else {
+            reject({success: false, message: "No token provided"});
+        }
+    });
+};
+
+module.exports.fetchDetails = (id) => {
+    return new Promise((resolve, reject) => {
+        StudentTransactions.findStudentById(id, (err, outputStudent) => {
+            if (err)
+                reject({success: false, message: "An error occurred"});
+            else {
+                outputStudent ? resolve({success: true, message: "Students details fetched", student: outputStudent}) : reject({success: false, message: "No such student found"});
+            }
+        });
+    });
+};
--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,3 +1,32 @@
 /**
  * Created by Yash 1300 on 16-04-2018.
  */
+
+const TeacherTransactions = require('../models/teacher/teacherTransactions');
+const jwt = require('jsonwebtoken');
+const Promise = require('bluebird');
+
+module.exports.verifyToken = (token, secret) => {
+    return new Promise((resolve, reject) => {
+        if (token) {
+            jwt.verify(token, secret, (err, decoded) => {
+                if (err)
+                    reject({success: false, message: "An error occurred"});
+                else
+                    decoded ? resolve(decoded) : reject({success: false, message: "Corrupted token provided"});
+            });
+        } else
+            reject({success: false, message: "No token provided"});
+    });
+};
+
+module.exports.fetchDetails = (id) => {
+    return new Promise((resolve, reject) => {
+        TeacherTransactions.findTeacherById(id, (err, outputTeacher) => {
+            if (err)
+                reject({success: false, message: "An error occurred"});
+            else
+                outputTeacher ? resolve({success: true, message: "Teacher details fetched", teacher: outputTeacher}) : reject({success: false, message: "No such teacher found"});
+        });
+    });
+};
