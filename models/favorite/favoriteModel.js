e8c935106724aea84740768491c56c7769d64e08
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-25 10:26:56 +0530

    Set up the whole schema for favorites

--- models/favorite/favoriteModel.js
+++ models/favorite/favoriteModel.js
@@ -1,3 +1,25 @@
 /**
  * Created by Yash 1300 on 22-05-2018.
  */
+
+const mongoose = require('mongoose');
+
+var favoriteSchema = new mongoose.Schema({
+    favoredByStudent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student'
+    },
+    favoriteTeacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher'
+    },
+    skill: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Skill'
+    }
+});
+
+// Making sure that there is no more than one favorite object containing same studentid, teacherid and skillid
+favoriteSchema.index({favoredByStudent: 1, favoriteTeacher: 1, skill: 1}, {unique: true});
+
+module.exports = mongoose.model('Favorite', favoriteSchema, "favorites");
