0b68a270356b0c715157cfcef91ac151b4191ead
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-11-05 00:06:45 +0530

    Added transactions for adding a skill

--- database/skill/skillTransactions.js
+++ database/skill/skillTransactions.js
@@ -7,3 +7,11 @@ const Skill = require('./skillModel');
 module.exports.fetchAllSkills = (next) => {
     Skill.find({}).exec(next);
 };
+
+module.exports.addASkill = (skill, next) => {
+    let newSkill = new Skill({
+        name: skill
+    });
+
+    newSkill.save(next);
+};
